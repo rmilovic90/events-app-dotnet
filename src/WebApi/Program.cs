9ce8ff1adc02b5048a7973bff26742f698211ba0
Rade Milovic <r.milovic90@gmail.com>
2026-02-23 12:22:19 +0100

    Configure global exception handling for web API.

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,12 +1,15 @@
 using Events.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 builder.Services.AddValidation();
 builder.Services.ConfigureOpenApi();
 builder.Services.ConfigureSecurity(builder.Configuration);
 builder.Services.ConfigureAppServices(builder.Configuration);
 
 var app = builder.Build();
+app.UseExceptionHandler();
 if (app.Environment.IsDevelopment())
 {
     app.UseOpenApi();
